44d9d4be8536cdcf05ea10afd05ba9a121e810bf
Reuben White <white.reuben@gmail.com>
2023-04-19 16:30:21 +1200

    added fix for AWS S3 refresh issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState, useParams, useRef } from "react";
 import { Provider, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router-dom'
 import { io } from "socket.io-client";
 
 
@@ -11,21 +12,29 @@ import { Chat } from "./components/chat/main/chat";
 
 function App() {
   
-  const router = useRouter();
-  const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
-  if (path) {
-    router.replace(path);
-  }
-  
-  const socket = io(process.env.REACT_APP_SERVER, {autoConnect: false});
-  console.log(`process.env...${process.env.REACT_APP_SERVER}`);
-  
+  const [socket, setSocket] = useState(null);
+
+  let navigate = useNavigate()
+  useEffect(() => {
+    const path  = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+    if (path) {
+      navigate(path, { replace: true});
+    }
+  },[])
+
+  useEffect(() => {
+    setSocket(io(process.env.REACT_APP_SERVER, {autoConnect: false}));
+    console.log(`process.env...${process.env.REACT_APP_SERVER}`);
+    return () => {
+      socket.disconnect();
+    };
+  })
 
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" exact element={<Join />} />
+          <Route path="/" element={<Join />} />
           <Route path="/chat/:room" element={<Chat socket={socket} />} />
         </Routes>
       </Router>
