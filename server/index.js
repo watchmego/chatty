77d40b047df44f65b2597e3ed2fc833f0e62271c
Reuben White <white.reuben@gmail.com>
2023-05-05 21:52:03 +1200

    added socketio error checking

--- server/index.js
+++ server/index.js
@@ -20,7 +20,11 @@ let typers = [];
 
 const sessionStore = new SessionStore();
 
-
+io.engine.on("connection_error", (err) => {
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
 
 io.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
